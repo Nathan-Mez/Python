2f899ce73fd18990dbbe24e1b80443d4de599b92
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-11-30 14:54:11 +0300

    Update recipe_oop.py

--- Exercise 1.5/recipe_oop.py
+++ Exercise 1.5/recipe_oop.py
@@ -3,72 +3,88 @@ class Recipe(object):
 
     def __init__(self, name):
         self.name = name
-        self.ingredients = []
         self.cooking_time = 0
-        self.difficulty = self.calculate_difficulty() 
+        self.ingredients = []
+        self.difficulty = None
 
+    # get name of recipe
     def get_name(self):
         return self.name
 
+    # get cooking time of recipe
     def get_cooking_time(self):
         return self.cooking_time
 
     def set_name(self, name):
         self.name = name
-
+   
+    # update cooking time of recipe
     def set_cooking_time(self, cooking_time):
         self.cooking_time = cooking_time
+        self.difficluty = self.calculate_difficulty()       # update recipe difficulty each time cooking time is altered
 
+    # add recipe ingredients 
     def add_ingredients(self, *args):
         for ingredient in args:
-            self.ingredients.append(ingredient)
-        self.update_all_ingredients()    
-        print('ingredients list: ', self.ingredients)  
+            self.ingredients.append(ingredient)   
+        self.difficluty = self.calculate_difficulty()       # update recipe difficulty each time ingredient is added 
+        self.update_all_ingredients()                       # update list of all ingredients 
 
-    def ingredients(self):
+    # get list of ingredients of the recipe in focus
+    def ingredients_list(self):
         return self.ingredients  
 
+    # calculate the recipe cooking difficulty
     def calculate_difficulty(self):
-        print('difficult ingredient: ', self.ingredients) 
-        if self.cooking_time < 10 and len(self.ingredients)< 4:
+        ingredients = self.ingredients_list()
+        if self.cooking_time < 10 and len(ingredients)< 4:
             self.difficulty = 'Easy'
-        elif self.cooking_time < 10 and len(self.ingredients) >= 4: 
+        elif self.cooking_time < 10 and len(ingredients) >= 4: 
             self.difficulty = 'Medium'
-        elif self.cooking_time >= 10 and len(self.ingredients) < 4:    
+        elif self.cooking_time >= 10 and len(ingredients) < 4:    
             self.difficulty = 'Intermediate'
-        elif self.cooking_time >= 10 and len(self.ingredients) >= 4:
+        elif self.cooking_time >= 10 and len(ingredients) >= 4:
             self.difficulty = 'Hard'       
         return self.difficulty    
 
+    # get the difficulty of recipe in focus
     def get_difficulty(self):
-        self.calc_difficulty()
+        self.calculate_difficulty()
         return self.difficulty
 
-    def search_ingredient(recipe, ingredient):
-        if ingredient in recipe.ingredients:
+    # search ingredient on recipe ingredients in focus
+    def search_ingredient(self, ingredient):
+        if ingredient in self.ingredients_list():
             return True
         else:
             return False
 
-    all_ingredients = []
+    # find recipes that contain specific ingredient
+    def recipe_search(data, search_term):
+        print('\nRecipes that include ingredient', search_term, '\n', 40 *'-') 
+        for recipe in data:
+            if recipe.search_ingredient(search_term):
+                print(recipe.name)
+               
+                                
 
+    all_ingredients = []          # set class variable to store list of all ingredients across all objects
+
+    # update list of all ingredients in 'all_ingredients' list
     def update_all_ingredients(self):
         if not self.ingredients == None:
             for ingredient in self.ingredients:
                 if ingredient not in self.all_ingredients:
                     self.all_ingredients.append(ingredient)
 
+    # string representaion for the Recipe class
     def __str__(self):
         output = '\nRecipe for ' + str(self.name) + '\n' + 20*'-' + '\nCooking Time (min) - ' + str(self.cooking_time) + '\nDifficulty - ' + str(self.difficulty) + '\nIngredients - \n'
         for ingredient in self.ingredients:
             output += "    " + ingredient + "\n"
         return output        
 
-    def recipe_search(self, data, search_term):
-        for recipe in data:
-            print(recipe)
-            if self.search_ingredient(recipe, search_term):
-                print(recipe)  
+                 
 
 #   python recipe_oop.py
 
@@ -94,7 +110,9 @@ banana_smoothie.set_cooking_time(5)
 print(banana_smoothie)
 
 recipes_list = [tea, coffee, cake, banana_smoothie]
-Recipe.recipe_search(recipes_list, 'Water')
+print(Recipe.recipe_search(recipes_list, 'Water'))
+print(Recipe.recipe_search(recipes_list, 'Sugar'))
+print(Recipe.recipe_search(recipes_list, 'Bananas'))
 
 
 
